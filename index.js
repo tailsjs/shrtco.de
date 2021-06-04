674b0d0a1478a9743d6fbf4d209f2832f084198d
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-06-04 15:50:38 +0400

    1.0.3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fetch = require("node-fetch");
-var FormData = require("form-data");
+	MODULE_VERSION = "1.3"
 class APIError extends Error {
 /**
 * @param {Object} params - Параметры ошибки
@@ -7,98 +7,100 @@ class APIError extends Error {
 * @param {String} params.message - Сообщение ошибки
 */
 constructor(params) {
+const { code, message } = params;
 
-super(params.message);
+super(message);
 
-this.code = params.code;
-this.message = params.message;
+this.code = code;
+this.message = message;
 this.name = this.constructor.name;
 
 Error.captureStackTrace(this, this.constructor);
 }
 }
-
-async function shorten(firstUrl) {
-	var url = encodeURI(firstUrl);
-	var result = (await (await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`)).json());
-	if (result.ok === true){
+async function getFic(id, random) {
+	var result = (await (await fetch(`https://ficbook-parser-server-part.herokuapp.com/fanfic?id=${id}${random ? `&random=true` : ""}`)).json())
+	if (result.success === true){
 		return result;
 	} else {
 		throw new APIError({
-			code: result.error_code,
+			code: 1,
 			message: result.error
 		});
-	}
-}
-async function info(someCode) {
-	var code = encodeURI(someCode);
-	var result = (await (await fetch(`https://api.shrtco.de/v2/info?code=${code}`)).json());
-	if (result.ok === true){
+	};	
+};
+async function getChangelogs() {
+	var result = (await (await fetch(`https://ficbook-parser-server-part.herokuapp.com/changelogs`)).json())
+	if (result.success === true){
+		if(result.MODULE.VERSION != MODULE_VERSION){
+			console.log(`Your version of the "ficbook-parser" module is out of date! We ask you to update it. NEW VERSION: ${result.MODULE.VERSION}, YOUR VERSION: ${MODULE_VERSION}`)
+		}
 		return result;
 	} else {
 		throw new APIError({
-			code: result.error_code,
+			code: 1,
 			message: result.error
 		});
+	};	
+};
+async function getAuthor(id, getComms) {
+	try{
+	var result = (await (await fetch(`https://ficbook-parser-server-part.herokuapp.com/authors?id=${id}${getComms ? `&comments=true` : ""}`)).json());
+	}catch(e){
+		throw new APIError({
+			code: 1,
+			message: "There is no such author!"
+		});	
 	}
-}
-
-async function customShorten(firstUrl, someCode) {
-	var url = encodeURI(firstUrl);
-	var code = encodeURI(someCode);
-	var form = new FormData();
-	form.append("url", url) ;
-	form.append("custom_code", code);
-	var result = (await (await fetch("https://api.shrtco.de/v2/shorten", {
-		method: "POST", 
-		body: form 
-	})).json());
-	if (result.ok === true) {
+	if (result.success === true){
 		return result;
 	} else {
 		throw new APIError({
-			code: result.error_code,
+			code: 1,
 			message: result.error
 		});
+	};
+};
+async function getFicPart(id, part) {
+	try{
+	var result = (await (await fetch(`https://ficbook-parser-server-part.herokuapp.com/ficparts?id=${id}&part=${part}`)).json());
+	}catch(e){
+		throw new APIError({
+			code: 1,
+			message: "There is no such part!"
+		});	
 	}
-}
-
-async function emojiCode(firstUrl) {
-	var url = encodeURI(firstUrl);
-	var result = (await (await fetch(`https://api.shrtco.de/v2/shorten?emoji&url=${url}`)).json());
-	if (result.ok === true){
+	if (result.success === true){
 		return result;
 	} else {
 		throw new APIError({
-			code: result.error_code,
+			code: 1,
 			message: result.error
 		});
+	};
+};
+async function getRequest(id) {
+	try{
+		var result = (await (await fetch(`https://ficbook-parser-server-part.herokuapp.com/requests?id=${id}`)).json());
+	}catch(e){
+		throw new APIError({
+			code: 1,
+			message: "There is no such request!"
+		});	
 	}
-}
-
-async function passShort(firstUrl, somePass) {
-	var url = encodeURI(firstUrl);
-	var pass = encodeURI(somePass);
-	var form = new FormData();
-	form.append("url", url) ;
-	form.append("password", pass);
-	var result = (await (await fetch("https://api.shrtco.de/v2/shorten", {
-		method: "POST", 
-		body: form 
-	})).json());
-	if (result.ok === true) {
+	if (result.success === true){
 		return result;
 	} else {
 		throw new APIError({
-			code: result.error_code,
+			code: 1,
 			message: result.error
 		});
-	}
-}
+	};
+};
 module.exports = {
-	shorten,
-	info,
-	customShorten,
-	emojiCode,
-	passShort
+	getFic,
+	getAuthor,
+	getChangelogs,
+	getRequest,
+	getFicPart
 };
