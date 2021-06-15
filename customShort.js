8d3f525feee6cfb6f349e6aa2994bdb844b51282
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-06-15 13:29:33 +0400

    Delete customShort.js

--- customShort.js
+++
@@ -1,9 +0,0 @@
-let shrtcode = require("./index.js"),
-    url = "example.com",
-    code = "shrtcode_example" + Math.round(Math.random() * 999999);
-
-shrtcode.customShorten(url, code)
-        .then(function(result){
-            console.log(result);
-        });
-
