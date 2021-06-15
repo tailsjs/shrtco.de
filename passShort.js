402b74b1c33145d1b3c87a1a233d4d9f5f21234c
tailsjs <49571184+tailsjs@users.noreply.github.com>
2021-06-15 13:29:47 +0400

    Delete passShort.js

--- passShort.js
+++
@@ -1,9 +0,0 @@
-let shrtcode = require("./index.js"),
-    url = "somesitewighrandomed"+ Math.round(Math.random() * 999999) + "numbersfortestthiscrapmodulefromtailsjs.com",
-    pass = "examplepass";
-
-shrtcode.passShort(url, pass)
-        .then(function(result){
-            console.log(result);
-        });
-
